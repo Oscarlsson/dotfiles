050c71b2eaa69c8928a59339dee362987c1fb861
Oscar Carlsson <ocarlsson3@gmail.com>
2013-12-23 10:39:48 +0100

    Add oh-my-zsh, update install script etc

--- install.sh
+++ install.sh
@@ -2,24 +2,36 @@
 #
 # This is meant to be a install-script.
 #
+# Install neccesary stuff 
+
+sudo apt-get install curl zsh ctags
 
 dir=~/dotfiles
 backupdir=~/olddot
-files="bashrc bash_aliases vimrc vim ssh xbindkeysrc dircolors"
+files="bashrc bash_aliases vimrc vim ssh dircolors xmonad xsession xmodbar oh-my-zsh zshrc gitconfig "
+
+# Xinitrc -> xsession?
+# Gnome-session-file
 
 mkdir -p $backupdir
-
 cd $dir
 
-#git submodule init
-#git submodule update
+git submodule init
+git submodule update
 
 # Create symlinks
-for files in $files; do
-    mv ~/.$file $backupdir
+for file in $files; do
+    
+    if [ -a ~/.$file ]; 
+        then
+        mv ~/.$file $backupdir
+    fi
+    
     ln -s $dir/.$file ~/.$file
 done
 
-
 # Set Solarized
 .solarized/gnome-terminal-colors-solarized/set_dark.sh
+
+# Install oh-my-zsh
+chsh -s /bin/zsh
